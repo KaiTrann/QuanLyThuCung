7ddf4189d9dd467c96b016a96d66ebe25c1d8d52
nguyenthuanh25052005-hub <nguyenthuanh25052005@gmail.com>
2026-02-25 14:19:08 +0700

    Add files via upload

---
+++ KhungApp.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nhóm_7
+{
+    public partial class KhungApp : Window
+    {
+        public KhungApp()
+        {
+            InitializeComponent();
+
+            try
+            {
+                var ver = Db.Scalar("SELECT VERSION();")?.ToString();
+                MessageBox.Show("Kết nối OK!\nMySQL Version: " + ver);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối:\n" + ex.Message);
+            }
+        }
+    }
+}
